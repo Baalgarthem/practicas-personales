2627d30ead18acdbe71790f1dd2e3721297d737a
Arlen <demonoid@outlook.jp>
2023-06-20 23:50:18 -0600

    Terminada la bd, intentando pruebas de conexion

---
+++ basesDatos/src/main/java/com/mycompany/basesdatos/inicio.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basesdatos;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Alush
+ */
+public class inicio {
+
+   public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        
+        try {
+            Connection pruebaConexion = conexion.get_connection();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
--- basesDatosJava/src/main/java/com/mycompany/basesdatosjava/BasesDatosJava.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.basesdatosjava;
-
-public class BasesDatosJava {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World! 2");
-    }
-}
