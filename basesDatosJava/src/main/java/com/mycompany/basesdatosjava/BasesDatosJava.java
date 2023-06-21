c24e7cef54c23341b064a5a80da9e574fb1cd5ff
Arlen <demonoid@outlook.jp>
2023-06-20 23:10:05 -0600

    nuevos cambios

--- basesDatosJava/src/main/java/com/mycompany/basesdatosjava/BasesDatosJava.java
+++ basesDatosJava/src/main/java/com/mycompany/basesdatosjava/BasesDatosJava.java
@@ -3,6 +3,6 @@ package com.mycompany.basesdatosjava;
 public class BasesDatosJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 2");
     }
 }
