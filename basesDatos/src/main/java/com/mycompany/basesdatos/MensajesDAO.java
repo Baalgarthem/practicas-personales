9f52c95a59831f6463a6089ecfae46e43a764c47
Arlen <demonoid@outlook.jp>
2023-06-21 02:31:59 -0600

    Podemos leer mensajes de la base de datos

--- basesDatos/src/main/java/com/mycompany/basesdatos/MensajesDAO.java
+++ basesDatos/src/main/java/com/mycompany/basesdatos/MensajesDAO.java
@@ -6,6 +6,7 @@ package com.mycompany.basesdatos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -37,6 +38,25 @@ public class MensajesDAO {
     }
 
     public static void leerMensaje() {
+        Conexion db_connect = new Conexion();
+        PreparedStatement declaracion =null;
+        ResultSet resultado = null;
+        try (Connection conexion = db_connect.get_connection()){
+            String query = "SELECT * FROM mensajes";
+            declaracion = conexion.prepareStatement(query);
+            resultado = declaracion.executeQuery();
+            
+            while (resultado.next()) {
+                System.out.println("ID: "+resultado.getInt("id_mensaje"));
+                System.out.println("Mensaje: "+resultado.getString("mensaje"));
+                System.out.println("Autor: "+resultado.getString("autor_mensaje"));
+                System.out.println("Fecha: "+resultado.getString("fecha_mensaje"));
+                System.out.println("----");
+            }
+        } catch (SQLException e) {
+            System.out.println("No se han podido leer los mensajes");
+            System.out.println(e);
+        }
     }
 
     public static void borrarMensaje(int id_mensaje) {
